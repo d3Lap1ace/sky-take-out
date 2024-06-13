dbfd9a3f0baa3e66555b52b64135877a2334f383
d3lap1ace L.Lyu <ap1cem@163.com>
2024-06-13 17:29:08 +0800

    Initial commit

---
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -0,0 +1,110 @@
+package com.sky.controller.admin;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.EmployeeService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 员工管理
+ */
+@RestController
+@RequestMapping("/admin/employee")
+@Slf4j
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 登录
+     *
+     * @param employeeLoginDTO
+     * @return
+     */
+    @PostMapping("/login")
+    @Operation(summary = "员工登录")
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+        log.info("员工登录：{}", employeeLoginDTO);
+
+        Employee employee = employeeService.login(employeeLoginDTO);
+
+        //登录成功后，生成jwt令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
+
+        return Result.success(employeeLoginVO);
+    }
+
+    /**
+     * 退出
+     *
+     * @return
+     */
+    @PostMapping("/logout")
+    public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 新增员工
+     */
+    @PostMapping()
+    @Operation(summary = "新增员工")
+    public Result saveEmp(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.saveEmp(employeeDTO);
+        return Result.success();
+    }
+
+    /**
+     * 员工分页查询
+     */
+    @GetMapping("/page")
+    @Operation(summary = "员工分页查询")
+    public Result pageQuery(EmployeePageQueryDTO employeePageQueryDTO){
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);//后续定义
+        return Result.success(pageResult);
+    }
+    /**
+     * 启用和禁用员工账号
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用和禁用")
+    public Result startAndBan(@PathVariable Integer status,Long id){
+        LambdaUpdateWrapper<Employee> employeeLambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+        employeeLambdaUpdateWrapper.eq(Employee::getId,id)
+                .set(Employee::getStatus,status);
+
+        return Result.success();
+    }
+}
